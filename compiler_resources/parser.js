2c4ca82898d8b8b6c5558c60240171cf88422ab5
tony <soricellia@gmail.com>
2018-03-04 22:55:38 -0500

    Fixed small typo in lexer. Parser works for most small programs. More test cases needed

--- compiler_resources/parser.js
+++ compiler_resources/parser.js
@@ -11,6 +11,11 @@
 ***************************************************************/
 var Tree = require('./tree');
 
+const firstOfStatement = new Set(["t_print", "t_while", "t_if" 
+						, "t_type", "t_openBrace", "t_char"]);
+
+const firstOfExpr = new Set(["t_digit", "t_string", "t_openParen", "t_char"]);
+
 function Parser(){
 	// the parse tree
 	this.tree = new Tree();
@@ -21,6 +26,7 @@ function Parser(){
 	this.tokens = {};
 	this.errors = [];
 
+	this.currentToken = null;
 
 // public functions
 
@@ -28,13 +34,11 @@ function Parser(){
 this.parseTokens = function(tokens, done){
 
 	this.tokens = tokens;
-	console.log("tokens: ", tokens)
 	this.errors.length = 0;
 	// start parsing our grammer
 	this.parseProgram();
 
 	if(!this.errors.length > 0){
-		console.log("here");
 		console.log(this.tree.toString());
 
 		// we made it here therefore we can just return the completed tree
@@ -48,81 +52,419 @@ this.parseTokens = function(tokens, done){
 this.parseProgram = function(){
 	// add the root node
 	this.tree.addNode("Program", "branch");
-
-	//using our ll1 grammer, we can look ahead one token.
-	var currentToken = this.getNext(this.tokens);
-	
-	console.log("current token: ", currentToken);
-
-	if(currentToken.type == "t_openBrace"){
-		this.match(currentToken);
-	}else{
-		this.errors.push("Error on line " + currentToken.linenumber 
-			+ ", expecting open bracket \"{\" character."
-			+ " Hint: Try starting your program with a \"{\" character");
-	}
+	console.log("ParseProgram()");
+	this.parseBlock();
+	this.kick();
 }
 
 this.parseBlock = function(){
+	console.log("ParseBlock()");
+
 	// since we're in a recursive mindfuck, we have to do
 	// things like this to bubble out of error case recursions
-	if(this.errors){
+	if(this.errors.length > 0){
 		console.log(this.errors);
 	}else{
+		this.tree.addNode("Block", "branch")
 
+		//using our ll1 grammer, we can look ahead one token.
+		this.currentToken = this.getNext(this.tokens);
+	
+		if(this.currentToken.type == "t_openBrace"){
+			// match {
+			this.match(this.currentToken);
+			this.parseStatementList();
+			this.kick();
+			this.currentToken = this.getNext();
+			if(this.currentToken.type == "t_closeBrace"){
+				this.match(this.currentToken);
+			}else{
+				this.errors.push("Error on line " + this.currentToken.linenumber 
+				+ ", expecting close bracket \"}\" character."
+				+ " Hint: did you forget a \"}\" character?");
+			}
+		}else{
+			//ERROR CASE
+			this.errors.push("Error on line " + this.currentToken.linenumber 
+				+ ", expecting open bracket \"{\" character."
+				+ " Hint: Try starting your program with a \"{\" character");
+		}
 	}
 }
 
-this.parseStatementList = function(tokens, done){
+this.parseStatementList = function(){
+	console.log("ParseStatementList()");
 
-}
 
-this.parseStatement = function(tokens, done){
-
-}
-
-this.parsePrintStatement = function(tokens, done){
-
-}
-
-this.parseAssignmentStatement = function(tokens, done){
-
-}
-
-this.parseVarDecl = function(tokens, done){
+	if(this.errors.length == 0){
+		this.tree.addNode("StatementList", "branch");	
+		
+		// get our current token
+		this.currentToken = this.getNext(this.tokens);
 	
+		// check if the current token is in fist of statement
+		if(firstOfStatement.has(this.currentToken.type)){
+			this.parseStatement();
+			this.kick();
+			this.parseStatementList();
+			this.kick();
+		}else{
+			// LAMBDA PRODUCTION
+		}
+	}else{
+		// we have errors
+		// since this is an lambda production, the error had to
+		// have came from somewhere else, so do nothing
+	}
 }
 
-this.parseWhileStatement = function(tokens, done){
-	
+this.parseStatement = function(){
+	console.log("ParseStatement()");
+
+	if(this.errors.length == 0){
+		this.tree.addNode("Statement", "branch");
+		if(this.currentToken.type == "t_print"){
+			this.parsePrintStatement();
+			this.kick();
+		}
+		
+		else if(this.currentToken.type == "t_char"){
+			this.parseAssignmentStatement();
+			this.kick();
+		}
+
+		else if(this.currentToken.type == "t_type"){
+			this.parseVarDecl();
+			this.kick();
+		}
+
+		else if(this.currentToken.type == "t_while"){
+			this.parseWhileStatement();
+			this.kick();
+		}
+
+		else if(this.currentToken.type == "t_if"){
+			this.parseIfStatement();
+			this.kick();
+		}
+
+		else if(this.currentToken.type == "t_openBrace"){
+			this.parseBlock();
+			this.kick();
+		}
+
+		else{
+			this.errors.push("Error on line " +
+					this.currentToken.linenumber +
+					". Expecting \"print\", \"int\", \"string\", \"boolean\""
+					+ ", \"variable_name\", \"while\", \"if\" or \"{\".");
+		}
+	}else{
+		// preexisting error case, bubble out of recursion
+	}
 }
 
-this.parseIfStatement = function(tokens, done){
-	
+this.parsePrintStatement = function(){
+	console.log("ParsePrintStatement()");
+	if(this.errors.length == 0){
+		
+		this.tree.addNode("PrintStatement", "branch");
+
+		if(this.nextToken.type == "t_print"){
+			// match PRINT
+			this.match(this.nextToken);
+			this.nextToken = this.getNext();
+			if(this.nextToken.type == "t_openParen"){
+				// match (
+				this.match(this.nextToken);
+
+				this.parseExpr();
+				this.kick();
+
+				this.nextToken = this.getNext();
+				if(this.nextToken.type == "t_closeParen"){
+					// match )
+					this.match(this.nextToken);
+				}else{
+					// error, missing ) after printexpr
+					this.errors.push("Error on line " +
+						this.currentToken.linenumber +
+						". Found " + this.currentToken.tokenValue +
+						" Expecting \")\" after print expr." +
+						" Hint: a print statement looks like print(expr)");
+				}
+			}else{
+				// error, missing ( after print
+				this.errors.push("Error on line " + 
+					this.currentToken.linenumber +
+					". Found "+ this.currentToken.tokenValue + 
+					" Expecting \"(\" after print statement");
+			}
+		}else{
+			this.errors.push("Error expecting print statement");
+		}
+	}else{
+		// preexisting error case, bubble out of recursion	
+	}
 }
 
-this.parseExpr = function(tokens, done){
-	
+this.parseAssignmentStatement = function(){
+	console.log("parseAssignmentStatement()");
+
+	if(this.errors.length == 0){
+		this.tree.addNode("AssignmentStatement", "branch");
+
+		if(this.currentToken.type == "t_char"){
+			// match ID
+			this.match(this.currentToken);
+
+			this.currentToken = this.getNext();
+
+			if(this.currentToken.type == "t_assignment"){
+				// match =
+				this.match(this.currentToken);
+				this.parseExpr();
+				this.kick();
+
+			}else{
+				// error, missing + 
+				this.errors.push("Error on line " +
+					this.currentToken.linenumber +
+					". Found " + this.currentToken.tokenValue +
+					" Expecting \"+\"");
+			}
+		}
+	}else{
+		// preexisting error case, bubble out of recursion	
+	}
 }
 
-this.parseIntExpr = function(tokens, done){
-	
+this.parseVarDecl = function(){
+	console.log("parseVarDecl()");
+
+	if(this.errors.length == 0){
+		this.tree.addNode("VarDecl", "branch");
+
+		//match type
+		this.match(this.currentToken);
+
+		this.currentToken = this.getNext();
+
+		if(this.currentToken.type == "t_char"){
+			this.parseId();
+			this.kick();
+		}else{
+			// error, expecting id
+			this.errors.push("Error on line" + 
+				this.currentToken.linenumber +
+				". Found " + this.currentToken.tokenValue +
+				" Expecting Variable ID. Hint: An ID is only one character, lowercase [a-z]");
+		}
+	}else{
+		// preexisting error case, bubble out of recursion	
+	}
 }
 
-this.parseStringExpr = function(tokens, done){
-	
+this.parseWhileStatement = function(){
+	console.log("parseWhileStatement()");
+
+	if(this.errors.length == 0){
+		this.tree.addNode("WhileStatement", "branch");
+
+		// match WHILE
+		this.match(this.currentToken);
+
+		this.parseBooleanExpr();
+		this.kick();
+		this.parseBlock();
+		this.kick();
+	}else{
+		// preexisting error case, bubble out of recursion	
+	}	
 }
 
-this.parseBooleanExpr = function(tokens, done){
-	
+this.parseIfStatement = function(){
+	console.log("parseIfStatement()");
+
+	if(this.errors.length == 0){
+		this.tree.addNode("IfStatement", "branch");
+
+		// match IF
+		this.match(this.currentToken);
+
+		this.parseBooleanExpr();
+		this.kick();
+		this.parseBlock();
+		this.kick();
+	}else{
+		// preexisting error case, bubble out of recursion	
+	}	
 }
 
-this.parseId = function(tokens, done){
+this.parseExpr = function(){
+	console.log("parseExpr()");
+	this.currentToken = this.getNext();
 	
+	if(this.errors.length == 0){
+		this.tree.addNode("Expr", "branch");
+		if(this.currentToken.type == "t_digit"){
+			this.parseIntExpr();
+			this.kick();
+		}else if(this.currentToken.type == "t_string"){
+			this.parseStringExpr();
+			this.kick();
+		}else if(this.currentToken.type == "t_openParen"){
+			this.parseBooleanExpr();
+			this.kick();
+		}else if(this.currentToken.type == "t_char"){
+			this.parseId();
+			this.kick();
+		}else{
+			this.errors.push("Error on line "+ this.currentToken.linenumber
+				+ ". Found " + this.currentToken.tokenValue + 
+				" Expecting digit, string character \"\"\" , open paren character \")\" or ID character [a-z]");
+		}
+		
+	}else{
+		// preexisting error case, bubble out of recursion	
+	}	
 }
 
-this.parseCharList = function(tokens, done){
+this.parseIntExpr = function(){
+	console.log("parseIntExpr()");
+
+	if(this.errors.length == 0){
+		this.tree.addNode("IntExpr", "branch");
+		// match digit
+		this.match(this.currentToken);
+
+		this.currentToken = this.getNext();
+
+		if(this.currentToken.type == "t_intop"){
+			// match +
+			this.match(this.currentToken);
+
+			this.parseExpr();
+			this.kick();
+
+		}else{
+			// lambda production
+		}
+	}else{
+		// preexisting error case, bubble out of recursion	
+	}	
+}
+
+this.parseStringExpr = function(){
+	console.log("parseStringExpr()")
 	
+	if(this.errors.length == 0){
+		this.tree.addNode("StringExpr", "branch");
+		
+		// match " 
+		this.match(this.currentToken);
+		this.parseCharList();
+		this.kick();
+
+		this.currentToken = this.getNext();
+		if(this.currentToken.type == "t_string"){
+			this.match(this.currentToken);
+		}else{
+			// error, expecting end of string "
+			this.errors.push("Error on line " + 
+				this.currentToken.linenumber + 
+				". Found " + this.currentToken.tokenValue +
+				" Expecting end of string \" character. Hint: \"xyz\" is a string.");	
+		}
+	}else{
+		// preexisting error case, bubble out of recursion	
+	}	
+}
+
+this.parseBooleanExpr = function(){
+	console.log("parseBooleanExpr()");
+	if(this.errors.length == 0){
+		this.tree.addNode("BooleanExpr", "branch");
+
+		this.currentToken = this.getNext();
+		// match (
+		this.match(this.currentToken);
+		
+		this.parseExpr();
+		this.kick();
+
+		this.currentToken = this.getNext();
+
+		if(this.currentToken.type == "t_boolop"){
+			// match boolop  
+			this.match(this.currentToken);
+
+			this.parseExpr();
+			this.kick();
+
+			this.currentToken = this.getNext();
+
+			if(this.currentToken.type == "t_closeParen"){
+				// match )
+				this.match(this.currentToken);
+			}else{
+				// error, expecting )
+				this.errors.push("Error on line " + 
+					this.currentToken.linenumber +
+					". Found " + this.currentToken.tokenValue +
+					" Expecting \")\" character. Hint: BooleanExpr looks like (Expr boolop Expr)");
+			}
+
+
+		}else{
+			// error, expecting +
+			this.errors.push("Error on line " + 
+					this.currentToken.linenumber +
+					". Found " + this.currentToken.tokenValue +
+					" Expecting \"\" character. Hint: BooleanExpr looks like (Expr boolop Expr)");
+		}
+	}else{
+		// preexisting error case, bubble out of recursion	
+	}	
+}
+
+this.parseId = function(){
+	console.log("parseId()")
+	
+	if(this.errors.length == 0){
+		this.tree.addNode("Id", "branch");
+		this.currentToken = this.getNext();
+		if(this.currentToken.type == "t_char"){
+			// match char
+			this.match(this.currentToken);	
+		}else{
+			// error, expecting char
+			this.errors.push("Error on line" + 
+					this.currentToken.linenumber +
+					". Found " + this.currentToken.tokenValue +
+					" Expecting ID. Hint: an ID is a single lowercase character [a-z]");	
+		}
+		
+	}else{
+		// preexisting error case, bubble out of recursion	
+	}	
+}
+
+this.parseCharList = function(){
+	if(this.errors.length == 0){
+		this.currentToken = this.getNext();
+
+		if(this.currentToken.type = "t_char"){
+			// match char
+			this.match(this.currentToken);
+			this.parseCharList();
+		}else{
+			// I DONT CARE ABOUT SPACES SO, 
+			// LAMBDA PRODUCTION
+		}
+	}else{
+		// preexisting error case, bubble out of recursion	
+	}	
 }
 
 
@@ -141,6 +483,13 @@ this.match = function(token){
 	this.tree.addNode(JSON.stringify(token.tokenValue), "leaf");
 	this.index++;
 }
+
+
+//kicks the tree one level up
+this.kick = function(){
+	this.tree.endChildren();
+}
+
 }
 
 module.exports = Parser;
