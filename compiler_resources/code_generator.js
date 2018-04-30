39e988596ca3a10c5757303bab8648ce340c7932
tony <soricellia@gmail.com>
2018-04-29 22:05:28 -0400

    code gen works for declaring and assigning integers

--- compiler_resources/code_generator.js
+++ compiler_resources/code_generator.js
@@ -34,6 +34,8 @@ var incrementByte = "EE";
 
 var sysCall = "FF";
 
+var nullTerminate = "00";
+
 // memory pointers
 var codeStartX = 0;
 var codeStartY;
@@ -44,13 +46,29 @@ var staticVarEnd;
 var heapStart;
 var heapEnd;
 
-var currentMemLoc;
+var currentMemLocX = 0;
+var currentMemLocY = 0;
+
+var staticsPointer = 0;
+var jumpPointer = 0;
+
+// our data structures for code generation
+var memory = [];
+var statics = [];
+var branches = [];
+var errors = [];
+var scope = -1;
+var symbolTable = [];
+
 
 // object that goes in statics 
 var static = function(temp, variable, address){
 	this.temp = temp;
 	this.variable = variable;
 	this.address = address;
+	this.addressx;
+	this.addressy;
+	this.value;
 }
 
 
@@ -62,39 +80,47 @@ var branch = function(temp, dist){
 
 
 function CodeGenerator(){
-	// our data structures for code generation
-	this.memory = [];
-	this.statics = [];
-	this.branches = [];
-	this.errors = [];
 
 	// initalize the memory addresses to all 0's 
 	var i, j;
 	for(i = 0; i < 16 ; i++){
-		this.memory[i] = [];
+		memory[i] = [];
 		for(j = 0 ; j < 16 ; j++){
-			this.memory[i][j] = 0;
+			memory[i][j] = "00";
 		}
 	}
 
 	// testing data structures
-	this.statics[0] = new static("t0xx", "a", "2F00");
-	this.branches[0] = new branch("J0", 7);
+	statics[0] = new static("t0xx", "a", "2F00");
+	branches[0] = new branch("J0", 7);
 	
-	console.log("memory: ", this.memory);
-	console.log("statics: ", this.statics);
-	console.log("branches", this.branches);
+	console.log("memory: ", memory);
+	console.log("statics: ", statics);
+	console.log("branches", branches);
 
 	/**********************
 		public functions
 	***********************/
-	this.generateCode = function(AST, symbolTable, done){
-		console.log(AST);
+	this.generateCode = function(AST, symTable, done){
 		var astRoot = AST.root;
 		var depth = 0;
+		symbolTable = symTable;
+		clearCodeGenerator();
 
 		traverseTree(astRoot, depth);
 
+		incrementMemY();
+		staticVarStart = [currentMemLocX, currentMemLocY];
+
+		//now we back patch
+		backPatchInts();
+
+		//we're finished, return our code
+        return done(errors, memory);
+
+		/************************
+			Private functions
+		************************/
 		function traverseTree(node, depth){
 			// Space out based on the current depth so
 			// this looks at least a little tree-like.
@@ -105,14 +131,15 @@ function CodeGenerator(){
 			// If there are no children (i.e., leaf nodes)...
 			if (!node || !node.children || node.children.length === 0) {
 				//console.log(node.name + " at depth " + depth);
-				console.log("here");
 			} 
 			// See what's kind of node we are currently on and decides which function to call
 			else {
 				console.log(node.name + " at depth " + depth);
 				
 				if (node.name == "Block"){
+					scope++;
 					traverseBlock(node.children, depth);
+					scope--;
 				}
 				else if (node.name == "Variable Declaration"){
 					traverseVarDecl(node, depth);
@@ -144,78 +171,253 @@ function CodeGenerator(){
 			        	traverseTree(node.children[i], depth + 1);
 			    	}
 				}
-			}	
-		}		
-	  	
-
-
-		//we're finished, return our code
-        return done(this.errors, this.memory);
-
+			}
+		}	
+				
         function traverseBlock(node, depth) {
-        	// Continues the traversal
-        	console.log("Generating Code For Block");
-        	for (var i = 0; i < node.length; i++) {
-                traverseTree(node[i], depth + 1);
-            }
-            //console.log("Program Finished codeTableLoc: " + codeTable.length);
+			// Continues the traversal
 
-            //return codeTable.length;
-        }
+			console.log("Generating Code For Block");
+			for (var i = 0; i < node.length; i++) {
+                traverseTree(node[i], depth + 1);
+			}
+		}
 
         function traverseVarDecl(node, depth){
         	console.log("Generating Code For Var Decl");
-        	for (var i = 0; i < node.length; i++) {
-                traverseTree(node[i], depth + 1);
-            }
+
+        	// 3 cases to worry about, int, boolean or string
+
+        			
+        	if(node.children[0].name == "int"){
+        			//load the acc with the int
+        			memory[currentMemLocX][currentMemLocY] = loadAccWithConst;
+        			incrementMemY();
+
+        			// now get the variable from the symbol table and lets add it to the statics table
+        			var symbolTableVariable = lookUpSymbolTableVariable(scope, node.children[1].name);
+        			
+        			var temp = "T" + staticsPointer;
+        			var staticVar = new static(temp,  
+        					symbolTableVariable ,
+        					"");
+        			
+        			statics[staticsPointer] = staticVar;
+        			staticsPointer++;
+        			
+        			// now default the value to 0 till we assign it something
+        			memory[currentMemLocX][currentMemLocY] = nullTerminate;
+       				incrementMemY();
+
+       				// store accumulator in memory
+       				memory[currentMemLocX][currentMemLocY] = storeAccInMem;
+       				incrementMemY();
+
+       				memory[currentMemLocX][currentMemLocY] = temp; 
+       				incrementMemY();
+
+       				memory[currentMemLocX][currentMemLocY] = "xx";
+       				incrementMemY();
+
+        	}
+        	else if(node.children[i].name == "boolean"){
+
+       		}
+       		else{ // string case
+
+       		}
         }
 
         function traverseAssign(node, depth){
+
         	console.log("Generating Code For Assignment");
-        	for (var i = 0; i < node.length; i++) {
-                traverseTree(node[i], depth + 1);
-            }
+        	
+        	//lookup variable in statics and assign it a value
+        	assignStaticsVariable(scope, node.children[0].name, node.children[1].name);
+        	
+
+        	//load accumulator with constant
+        	memory[currentMemLocX][currentMemLocY] = loadAccWithConst;
+        	incrementMemY();
+
+        	// check whether to 0 pad or not
+        	if(node.children[1].name.toString(16).length < 2){ 
+        		memory[currentMemLocX][currentMemLocY] = "0" + node.children[1].name.toString(16);
+        		
+        	}else{
+        		memory[currentMemLocX][currentMemLocY] = node.children[1].name.toString(16);
+        		
+        	}
+        	incrementMemY();
+
+        	//store accumlator in memory
+        	var temp = lookUpStaticsVariable(scope, node.children[0].name).temp;
+        	
+        	memory[currentMemLocX][currentMemLocY] = storeAccInMem;
+        	incrementMemY();
+
+        	memory[currentMemLocX][currentMemLocY] = temp;
+        	incrementMemY();
+
+        	memory[currentMemLocX][currentMemLocY] = "xx";
+        	incrementMemY();
+
         }
+        
         function traversePrint(node, depth){
         	console.log("Generating Code For Print");
         	for (var i = 0; i < node.length; i++) {
                 traverseTree(node[i], depth + 1);
             }
         }
+        
         function traverseIf(node, depth){
         	console.log("Generating Code For If");
         	for (var i = 0; i < node.children.length; i++) {
                 traverseTree(node.children[i], depth + 1);
             }
         }
+
         function traverseWhile(node, depth){
         	console.log("Generating Code For While");
         	for (var i = 0; i < node.children.length; i++) {
                 traverseTree(node.children[i], depth + 1);
             }
         }
+
         function traverseAdd(node, depth){
         	console.log("Generating Code For Add");
         	for (var i = 0; i < node.length; i++) {
                 traverseTree(node[i], depth + 1);
             }
         }
+
         function traverseEquals(node, depth){
         	console.log("Generating Code For Equals");
         	for (var i = 0; i < node.length; i++) {
                 traverseTree(node[i], depth + 1);
             }	
         }
+
         function traverseNotEquals(node, depth){
         	console.log("Generating Code For Not Equals");
         	for (var i = 0; i < node.length; i++) {
                 traverseTree(node[i], depth + 1);
             }	
         }
-    }
 
+        function incrementMemY(){
+        	currentMemLocY ++;
+        	if(currentMemLocY == 16){
+        		currentMemLocY = 0;
+        		incrementMemX();
+        	}
+        }
 
+        function incrementMemX(){
+        	currentMemLocX ++;
+        	if(currentMemLocX == 16){
+        		errors.add("Error, out of memory address. Program too large.");
+        	}
+        }
 
+        function lookUpSymbolTableVariable(scope,variable){
+        	var i,j, returnVal;
+        	
+        	// start at current scope and then move backwards
+        	for(i = scope; i >= 0; i--){
+        		for(j = 0; j < symbolTable[i].length; j++){
+        			if(symbolTable[i][j].name == variable){
+        				returnVal = symbolTable[i][j];
+        			}
+        		}
+        	}
+
+        	return returnVal;
+        }
+
+        function lookUpStaticsVariable(scope, variable){
+        	// first find the variable in the symbol table
+        	// for speed were going to do a little cheat and make the object into JSON
+        	// this allows us to compare objects ;D
+        	
+        	var symbTableVar = JSON.stringify(lookUpSymbolTableVariable(scope, variable));
+
+        	//now we look for the symbTableVar in statics and return it
+        	var i;
+        	for(i = 0; i < statics.length; i++){
+        		// this is a nice little cheat to compare objects
+        		if(JSON.stringify(statics[i].variable) == symbTableVar){
+   					return statics[i];     			
+        		}
+        	}
+        }
+
+        function assignStaticsVariable(scope, variable, value){
+        	// first find the variable in the symbol table
+        	// for speed were going to do a little cheat and make the object into JSON
+        	// this allows us to compare objects ;D
+        	var symbTableVar = JSON.stringify(lookUpSymbolTableVariable(scope, variable));
+
+        	//now we look for the symbTableVar in statics and return it
+        	var i;
+        	for(i = 0; i < statics.length; i++){
+        		// this is a nice little cheat to compare objects
+        		if(JSON.stringify(statics[i].variable) === symbTableVar){
+   					statics[i].value = value;     			
+        		}
+        	}
+        }
+
+        function backPatchInts(){
+        	var i, x, y;
+        	var temp;
+        	getIntAddresses();
+        	for(i=0; i<statics.length;i++){
+        		temp = statics[i].temp; //this is our temp number i.e. T0
+        		for(x = 0; x < memory.length; x++){
+        			for(y = 0; y < memory[x].length; y++){
+        				if(memory[x][y] == temp){
+        					// back patch
+        					memory[x][y] = statics[i].address;
+
+        					if(y == 16){
+        						memory[x+1][0] = "00";
+        					}else{
+        						memory[x][y+1] = "00";
+        					}
+        				}
+        			}
+        		}
+        	}
+        }
+
+        // goes through the statics table and assigns them all an address
+        function getIntAddresses(){
+        	var i;
+        	for(i=0;i<statics.length; i++){
+        		statics[i].address = currentMemLocX.toString(16).toUpperCase() + currentMemLocY.toString(16).toUpperCase();
+    			incrementMemY();
+        	}
+        }
+        function clearCodeGenerator(){
+			// initalize the memory addresses to all 0's 
+			var i, j;
+			for(i = 0; i < 16 ; i++){
+				memory[i] = [];
+				for(j = 0 ; j < 16 ; j++){
+					memory[i][j] = "00";
+				}
+			}
+			currentMemLocX = 0;
+			currentMemLocY = 0;
+			scope = -1;
+			staticsPointer = 0;
+			jumpPointer = 0;
+			statics = [];
+			branches = [];
+        }
+	}
 }
 
 module.exports = CodeGenerator;
